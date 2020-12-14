3a7a9288c29759196d32da8a6d12abccac785ffa
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2020-12-14 17:45:22 +0300

    add decription of the function

--- src/script.js
+++ src/script.js
@@ -1,3 +1,7 @@
+/*При нажатии на "About UI Lab" в навигации 
+появляется всплывающее окно с текстом
+"Some information about UI lab"
+*/
 window.onload = function() {
     const click = document.getElementById('item-1-click');
     click.onclick = function() { 
