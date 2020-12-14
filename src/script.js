9e6f1169a5e379c47cfc4ecd05657ef30eddd292
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2020-12-14 14:34:45 +0300

    add script

---
+++ src/script.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    const click = document.getElementById('item-1-click');
+    click.onclick = function() { 
+        alert('Some information about UI lab.');
+    }
+}
